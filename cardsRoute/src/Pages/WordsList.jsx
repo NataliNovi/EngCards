0ded0e36ab55e464fb6cf4a7dea5fffe8e2d22e5
Natalia Novikova <106342111+NataliNovi@users.noreply.github.com>
2023-03-02 17:15:51 +0300

    target.value

--- cardsRoute/src/Pages/WordsList.jsx
+++ cardsRoute/src/Pages/WordsList.jsx
@@ -23,6 +23,7 @@ import Footer from '../components/Footer/Footer';
   
     const handleSaveWordChange = () => {
       setIsInputShow(!isInputShow)
+      
     }
 
   
@@ -53,15 +54,46 @@ const [words, setWords] = useState(wordsList);
   })}
 
 
+  const [valueId, setValueId] = useState('');
+  const onChangeValueId = (e) => {
+      //setValueId(e.target.value);
+      if (valueId > 0) {
+          return valueId
+      }
+      else return 'error' ;
+      
+  }
+  //console.log(valueId)
+
+  const [valueEng, setValueEng] = useState('');
+  const onChangeValueEng = (e) => {
+      //setValueEng(e.target.value);
+      console.log(valueEng)
+  }
+
+  const [valueTranscript, setValueTranscript] = useState('');
+  const onChangeValueTranscript = (e) => {
+      //setValueTranscript(e.target.value);
+      //console.log(valueTranscript)
+  }
+
+  const [valueTranslate, setValueTranslate] = useState('');
+  const onChangeValueTranslate = (e) => {
+      //setValueTranslate(e.target.value);
+      //console.log(valueTranslate)
+  }
+
+
+
 
     return (
           <div className={styles.wordsListTableContainer}>
              
         {isInputShow
-             ? <Input handleCancelWordChange = {handleCancelWordChange} handleSaveWordChange = {handleSaveWordChange}/>
+             ? <Input handleCancelWordChange = {handleCancelWordChange} handleSaveWordChange = {handleSaveWordChange} onChangeValueId={onChangeValueId} onChangeValueEng={onChangeValueEng} onChangeValueTranscript={onChangeValueTranscript} onChangeValueTranslate={onChangeValueTranslate} value={[valueId, valueEng, valueTranscript, valueTranslate]}  />
              : <div className={styles.wordsList}>
             {wordsList.map((item)=>
-                    <Table key={item.id} id={item.id} english={item.english} transcription={item.transcription} russian={item.russian} tags={item.tags} isActive = {item.isActive} index = {item.index} handleCorrectWord = {handleCorrectWord} handleDeleteWord = {handleDeleteWord} />
+                <Table key={item.id} id={item.id} english={item.english} transcription={item.transcription} russian={item.russian} tags={item.tags} isActive = {item.isActive} index = {item.index} handleCorrectWord = {handleCorrectWord} handleDeleteWord = {handleDeleteWord} />
                   )}
             </div>} 
 
